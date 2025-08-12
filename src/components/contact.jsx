51a6d07121106fb5fb2a304288f321ff28a2f352
Chance Kondowe <chancekondowe@gmail.com>
2025-08-12 09:40:51 +0200

    update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -25,7 +25,7 @@ export const Contact = (props) => {
       .sendForm(
         "service_k7h0uy9", // service ID
         "template_5hoacbf", // template ID
-        formRef.current,   // <-- use ref here
+        formRef.current,    // form reference
         "7XuusxucPT37sZGoX" // public key
       )
       .then(
@@ -40,31 +40,136 @@ export const Contact = (props) => {
   };
 
   return (
-    <form ref={formRef} onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="user_name" // <-- must match template variable in EmailJS
-        placeholder="Name"
-        value={name}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="email"
-        name="user_email" // <-- must match template variable
-        placeholder="Email"
-        value={email}
-        onChange={handleChange}
-        required
-      />
-      <textarea
-        name="message" // <-- must match template variable
-        placeholder="message"
-        value={message}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Send</button>
-    </form>
+    <div id="contact">
+      <div className="container">
+        <div className="col-md-8">
+          <div className="row">
+            <div className="section-title">
+              <h2>Get In Touch</h2>
+              <p>
+                Please fill out the form below to send us an email and we will
+                get back to you as soon as possible.
+              </p>
+            </div>
+
+            {/* Email Form */}
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      name="user_name" // must match EmailJS template var
+                      className="form-control"
+                      placeholder="Name"
+                      value={name}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      name="user_email" // must match EmailJS template var
+                      className="form-control"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="form-group">
+                <textarea
+                  name="message"
+                  className="form-control"
+                  rows="4"
+                  placeholder="Message"
+                  value={message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+              </div>
+
+              <button type="submit" className="btn btn-custom btn-lg">
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+
+        {/* Contact Info */}
+        <div className="col-md-3 col-md-offset-1 contact-info">
+          <div className="contact-item">
+            <h3>Contact Info</h3>
+            <p>
+              <span>
+                <i className="fa fa-map-marker"></i> Address
+              </span>
+              {props.data?.address || "loading"}
+            </p>
+          </div>
+          <div className="contact-item">
+            <p>
+              <span>
+                <i className="fa fa-phone"></i> Phone
+              </span>{" "}
+              {props.data?.phone || "loading"}
+            </p>
+          </div>
+          <div className="contact-item">
+            <p>
+              <span>
+                <i className="fa fa-envelope-o"></i> Email
+              </span>{" "}
+              {props.data?.email || "loading"}
+            </p>
+          </div>
+        </div>
+
+        {/* Social Links */}
+        <div className="col-md-12">
+          <div className="row">
+            <div className="social">
+              <ul>
+                <li>
+                  <a href={props.data?.facebook || "/"}>
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href={props.data?.twitter || "/"}>
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href={props.data?.youtube || "/"}>
+                    <i className="fa fa-youtube"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      {/* Footer */}
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; {new Date().getFullYear()} All rights reserved, designed by{" "}
+            <a href="Ellie&Char LTD" rel="nofollow">
+              Ellie&Char LTD
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
